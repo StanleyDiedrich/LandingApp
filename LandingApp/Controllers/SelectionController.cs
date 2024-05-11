b8c6f65826440287b7dc5abd8e922fe2bfc18a35
stanislavdidenko <didenko.sid@yandex.ru>
2024-05-11 13:56:11 +0300

    it started to break down

--- LandingApp/Controllers/SelectionController.cs
+++ LandingApp/Controllers/SelectionController.cs
@@ -14,26 +14,21 @@ namespace LandingApp.Controllers
         {
             return View();
         }
-
         [HttpPost]
-        public IActionResult UploadFile(IFormFile file)
+        public async Task<IActionResult> UploadFile(IFormFile file)
         {
-            if (file != null && file.Length > 0)
+            // Добавьте здесь код для загрузки файла
+            if (file!=null && file.Length>0)
             {
                 var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/upload", file.FileName);
                 using (var stream = new FileStream(filePath, FileMode.Create))
                 {
                     file.CopyTo(stream);
                 }
-                ViewBag.Message = "File uploaded successfully";
             }
-            else
-            {
-                ViewBag.Message = "File not selected or empty";
-            }
-
             return View();
         }
+        
     }
 }
 
---
+++ LandingApp/Controllers/UploadController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LandingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : Controller
+    {
+
+
+
+        // GET api/upload/getfile
+        [HttpGet("getfile")]
+        public void GetFile()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/upload");
+            var files = Directory.GetFiles(path);
+
+             RunFile(files);
+            return;
+        }
+
+        [HttpPost("runfile")]
+        public IActionResult RunFile([FromBody] string[] files)
+        {
+            foreach(var file in files)
+            {
+                Process.Start(file);
+            }    
+            
+
+            return Ok("File started successfully");
+        }
+    }
+}
+
+
+
+
